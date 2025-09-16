68b9ef904f90f2c97fde13ab23c45c7c84fc3b0e
Marlos Leles <marlosleles@gmail.com>
2025-09-16 13:49:00 -0300

    Updated Exercise 5.py

--- 1 - Develop Gen AI Apps/Module 2/Exercise 5.py
+++ 1 - Develop Gen AI Apps/Module 2/Exercise 5.py
@@ -11,3 +11,16 @@ ai_response = chat.invoke(history.messages)
 print(ai_response)
 
 history.add_ai_message(ai_response)
+
+# Conversational buffer
+from langchain.memory import ConversationBuffermemory
+from langchain.chains import ConversationChain
+
+conversation = ConversationChain(
+  llm=llama_llm,
+  verbose=True,
+  memory=ConversationBufferMemory()
+)
+conversation.invoke(input="Hello, I am a little cat. Who are you?")
+conversation.invoke(input="What can you do?")
+conversation.invoke(input="Who am I?")
