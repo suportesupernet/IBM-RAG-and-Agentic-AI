c1e3bfc50e7547ea245ef96ff512cacbdc74a996
Marlos Leles <marlosleles@gmail.com>
2025-10-05 10:43:44 -0300

    capital.py updated

--- 1 - Develop Gen AI Apps/Module 3/capital.py
+++ 1 - Develop Gen AI Apps/Module 3/capital.py
@@ -1,3 +1,13 @@
 from ibm_watsonx_ai import Credentials
 from ibm_watsonx_ai.foundation_models import ModelInference
 from ibm_watsonx_ai.metanames import GenTextParamsMetaNames
+
+credentials = Credentials(
+    url = "https://us-south.ml.cloud.ibm.com",
+    # api_key = "<YOUR-API-KEY>"
+)
+
+params = {
+    GenTextParamsMetaNames.DECODING_METHOD: "greedy",
+    GenTextParamsMetaNames.MAX_NEW_TOKENS: 100
+    }
